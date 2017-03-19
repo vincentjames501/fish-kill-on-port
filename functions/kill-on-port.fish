1372bb94d231418b4b7b5183a65f7a0efd2f0719
Vincent Pizzo <vincent.pizzo@singlewire.com>
2017-03-18 21:03:11 -0500

    Don’t rely on getopts

--- functions/kill-on-port.fish
+++ functions/kill-on-port.fish
@@ -1,11 +1,12 @@
 # SYNOPSIS
-#   kill-on-port [OPTION]... PORTS...
+#   kill-on-port [OPTION]...
 #
 # USAGE
 #   Options
 #       -l, --list     List processes using the specified ports (does not kill)
 #       -r, --root     Attempts to kill the process as root
 #       -f, --force    Does not ask for confirmation
+#       -p, --port     The port to to look at (one or more required)
 #       -h, --help     Prints usage
 
 function init -a path --on-event init_kill-on-port
@@ -17,29 +18,33 @@ function kill-on-port -d "Kills a process that is using the given port"
   set -l list 0
   set -l root ""
 
-  while set optarg (getopts "f:force h:help r:root l:list" $argv)
-    switch $optarg[1]
-      case f
+  for i in (echo $argv | sed 's|--*|\\'\n'|g' | grep -v '^$')
+    echo $i | read -l option value
+
+    switch $option
+      case f force
         set force 1
-      case r
+      case r rot
         set root "sudo "
-      case l
+      case l list
         set list 1
+      case p port
+        set -l ports $ports $value
       case h
         __kill-on-port.print-usage
         return
       case \*
-        echo -e "Unknown option $optarg[1]\n"
+        echo -e "Unknown option $option\n"
         __kill-on-port.print-usage
         return
     end
   end
 
-  if test (count $optarg) = 0
+  if test (count $ports) = 0
     echo -e "One or more ports must be specified\n"
     __kill-on-port.print-usage
   else
-    for port in $optarg
+    for port in $ports
       set -l process (lsof -i -P | grep $port)
       if test -n "$process"
         set -l name (echo $process | awk '{print $1}')
